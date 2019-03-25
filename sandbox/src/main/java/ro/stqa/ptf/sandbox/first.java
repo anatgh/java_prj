8a7ffff31f8e3957141d6544b1ee909bb3c249ab
selena <selena@agepi.md>
2019-03-25 09:49:37 +0200

    arifmeti4eskie operatsii

--- sandbox/src/main/java/ro/stqa/ptf/sandbox/first.java
+++ sandbox/src/main/java/ro/stqa/ptf/sandbox/first.java
@@ -1,7 +1,18 @@
 package ro.stqa.ptf.sandbox;
 
 public class first {
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
+  public static void main(String[] args) {
+    System.out.println("hello");
+
+    System.out.println(2 + 2);
+    System.out.println(2 * 2);
+    System.out.println(2 / 2);
+    System.out.println(2 - 2);
+
+    System.out.println(1.0 / 2);
+
+    System.out.println("2" + "2");
+    System.out.println("2 + 2 = " + (2 + 2));
+
+  }
 }
