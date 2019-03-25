a0e17bf86dc6a6ef3c3c351b75220ab04de20af3
selena <selena@agepi.md>
2019-03-25 10:55:59 +0200

    Added some functions

--- sandbox/src/main/java/ro/stqa/ptf/sandbox/first.java
+++ sandbox/src/main/java/ro/stqa/ptf/sandbox/first.java
@@ -2,17 +2,27 @@ package ro.stqa.ptf.sandbox;
 
 public class first {
   public static void main(String[] args) {
-    System.out.println("hello");
+    hello("world");
+    hello("user");
+    hello("Selena");
 
-    System.out.println(2 + 2);
-    System.out.println(2 * 2);
-    System.out.println(2 / 2);
-    System.out.println(2 - 2);
+    double len = 5;
+    System.out.println("Plos4adi kvadrata so storonoi " + len + " = " + area(len));
 
-    System.out.println(1.0 / 2);
+    double a = 4;
+    double b = 6;
+    System.out.println("Plos4adi preamougolinika so storonami " + a + " and " + b + " = " + area(a,b));
+  }
 
-    System.out.println("2" + "2");
-    System.out.println("2 + 2 = " + (2 + 2));
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
 
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 }
